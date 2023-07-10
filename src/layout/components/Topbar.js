2f3b8a777f4a10bc198d2b899b2e228d1bd8d263
geofffactorypure <gjarman@factorypure.com>
2023-07-10 10:59:38 -0500

    adding max width

--- src/layout/components/Topbar.js
+++ src/layout/components/Topbar.js
@@ -1,36 +1,38 @@
 export default function Topbar() {
     return (
-        <nav class="shadow-small bg-darkaccent px-[36px] py-[12px] flex gap-[64px] items-center">
-            <a class="font-bai font-medium text-[20px] text-offwhite block w-fit" href="/">
-                <p class="leading-[1]">FactoryPure</p>
-                <p class="font-light text-accent leading-[1] flex justify-between">
-                    <span>D</span>
-                    <span>e</span>
-                    <span>v</span>
-                    <span>e</span>
-                    <span>l</span>
-                    <span>o</span>
-                    <span>p</span>
-                    <span>e</span>
-                    <span>r</span>
-                    <span>s</span>
-                </p>
-            </a>
-            <ul class="text-offwhite font-bold flex items-center gap-[32px]">
-                <li>
-                    <a href="/edi">
-                        EDI
-                    </a>
-                </li>
-                <li>
-                    <a href="/api">
-                        API
-                    </a>
-                </li>
-            </ul>
-            <a class="uppercase ml-auto py-[4px] px-[16px] bg-accent rounded-[4px] text-darkness font-bold" href="/contact">
-                Contact
-            </a>
+        <nav class="shadow-small bg-darkaccent">
+            <div class="max-w-[1440px] mx-auto flex gap-[64px] items-center px-[36px] py-[12px]">
+                <a class="font-bai font-medium text-[20px] text-offwhite block w-fit" href="/">
+                    <p class="leading-[1]">FactoryPure</p>
+                    <p class="font-light text-accent leading-[1] flex justify-between">
+                        <span>D</span>
+                        <span>e</span>
+                        <span>v</span>
+                        <span>e</span>
+                        <span>l</span>
+                        <span>o</span>
+                        <span>p</span>
+                        <span>e</span>
+                        <span>r</span>
+                        <span>s</span>
+                    </p>
+                </a>
+                <ul class="text-offwhite font-bold flex items-center gap-[32px]">
+                    <li>
+                        <a href="/edi">
+                            EDI
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/api">
+                            API
+                        </a>
+                    </li>
+                </ul>
+                <a class="uppercase ml-auto py-[4px] px-[16px] bg-accent rounded-[4px] text-darkness font-bold" href="/contact">
+                    Contact
+                </a>
+            </div>
         </nav>
     )
 }
