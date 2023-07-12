21ab517c0e31ea6e381e0f0c859ddfb7e91dd56c
Gabriel-Bigelow <gabebigelow@gmail.com>
2023-07-12 09:27:48 -0400

    get tailwind working, start 997

--- src/edi/997/components/Overview.js
+++ src/edi/997/components/Overview.js
@@ -37,9 +37,9 @@ export default function Overview() {
                     <div></div>
                     <div></div>
                 </div>
-                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
-                    <div>BIA</div>
-                    <div>Beginning segment for inventory advice</div>
+                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] border-darkgrey">
+                    <div>AK1</div>
+                    <div>Functional Group Response Header</div>
                     <div>M</div>
                     <div>1</div>
                     <div></div>
@@ -53,8 +53,24 @@ export default function Overview() {
                     <div>Segment Name</div>
                     <div>Req</div>
                     <div>Max Use</div>
-                    <div>Repeat</div>
+                    <div>LOOP</div>
                     <div>Notes</div>
+                </div>
+                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] border-b border-darkgrey">
+                    <div>AK2</div>
+                    <div>Transaction Set Response Header</div>
+                    <div>O</div>
+                    <div>1</div>
+                    <div>AK2</div>
+                    <div>N1/0300L</div>
+                </div>
+                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] border-b border-darkgrey">
+                    <div>AK5</div>
+                    <div>Transaction Set Response Trailer</div>
+                    <div>O</div>
+                    <div>1</div>
+                    <div>AK2</div>
+                    <div></div>
                 </div>
             </div>
             <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Summary</h2>
@@ -68,16 +84,8 @@ export default function Overview() {
                     <div>Notes</div>
                 </div>
                 <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
-                    <div>CTT</div>
-                    <div>Transaction totals</div>
-                    <div>M</div>
-                    <div>1</div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
-                    <div>GE</div>
-                    <div>Functional Group Trailer</div>
+                    <div>AK9</div>
+                    <div>Functional Group Response Trailer</div>
                     <div>M</div>
                     <div>1</div>
                     <div></div>
@@ -92,12 +100,63 @@ export default function Overview() {
                     <div></div>
                 </div>
                 <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
+                    <div>GE</div>
+                    <div>Functional Group Trailer</div>
+                    <div>M</div>
+                    <div>1</div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
                     <div>IEA</div>
                     <div>Interchange Control Trailer</div>
                     <div>M</div>
                     <div>1</div>
                     <div></div>
                     <div></div>
+                </div>
+            </div>
+            <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Notes</h2>
+            <div className="text-offwhite grid grid-cols-[1fr_200px_1fr_1fr_1fr_1fr_1fr] gap-x-[16px] mb-[32px]">
+                <div className="text-offwhite grid grid-cols-[100px_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
+                    <p>1/0100</p>
+                    <p>
+                        These acknowledgments shall not be acknowledged, thereby preventing an endless cycle of
+                        acknowledgments of acknowledgments. Nor shall a Functional Acknowledgment be sent to report errors
+                        in a previous Functional Acknowledgment.
+                        <br/>
+                        The Functional Group Header Segment (GS) is used to start the envelope for the Functional
+                        Acknowledgment Transaction Sets. In preparing the functional group of acknowledgments, the applicationsender's code and the application receiver's code, taken from the functional group being acknowledged,
+                        are exchanged; therefore, one acknowledgment functional group responds to only those functional groups
+                        from one application receiver's code to one application sender's code.
+                        There is only one Functional Acknowledgment Transaction Set per acknowledged functional group.
+                    </p>
+                </div>
+                <div className="text-offwhite grid grid-cols-[100px_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
+                    <p>1/0200</p>
+                    <p>
+                        AK1 is used to respond to the functional group header and to start the acknowledgment for a functional
+                        group. There shall be one AK1 segment for the functional group that is being acknowledged.
+                        The Functional Acknowledgement is generated at the point of translation, intended for the originator (not
+                        any intermediate parties).
+
+                    </p>
+                </div>
+                <div className="text-offwhite grid grid-cols-[100px_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
+                    <p>1/0300</p>
+                    <p>
+                        AK2 is used to start the acknowledgment of a transaction set within the received functional group. The
+                        AK2 segments shall appear in the same order as the transaction sets in the functional group that has
+                        been received and is being acknowledged.
+                    </p>
+                </div>
+                <div className="text-offwhite grid grid-cols-[100px_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
+                    <p>1/0300</p>
+                    <p>
+                        AK2 is used to start the acknowledgment of a transaction set within the received functional group. The
+                        AK2 segments shall appear in the same order as the transaction sets in the functional group that has
+                        been received and is being acknowledged. 
+                    </p>
                 </div>
             </div>
         </div>
--- src/edi/components/Acknowledgements.js
+++ src/edi/components/Acknowledgements.js
@@ -1,8 +1,8 @@
-export default function SenderReceiverIds() {
+export default function Acknowledgements() {
     return (
         <div id="ids">
-            <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Sender / Receiver IDs</h2>
-            <p className="text-offwhite mb-[8px]">We prefer a mutually defined id so we can make distinctions between test and production messages. However, you may also use your DUNS number or telephone number.</p>
+            <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Acknowledgements</h2>
+            <p className="text-offwhite mb-[8px]">These acknowledgments shall not be acknowledged, thereby preventing an endless cycle of acknowledgments of acknowledgments. Nor shall a Functional Acknowledgment be sent to report errors in a previous Functional Acknowledgment.</p>
             <p className="text-offwhite mb-[8px]">When testing, we use the sender ID:</p>
             <p className="text-offwhite mb-[8px] ml-[8px] font-bold">FACTORYPUREEDIT</p> 
             <p className="text-offwhite mb-[8px]">In Production, we use the sender ID:</p>
