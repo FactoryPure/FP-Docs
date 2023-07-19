36b0aa17e725ea107b96b7360a943bfd56fbdf75
geofffactorypure <gjarman@factorypure.com>
2023-07-19 14:31:38 -0500

    fixing typo

--- src/edi/846/components/LIN.js
+++ src/edi/846/components/LIN.js
@@ -68,7 +68,7 @@ export default function LIN() {
                     <div>C</div>
                     <div>ID</div>
                     <div>2/2</div>
-                    <div>UP</div>
+                    <div>SK</div>
                 </div>
                 <div className="text-offwhite grid grid-cols-[1fr_1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
                     <div>LIN05</div>
