c63de3737b520c0fa9a8fcc1f58bf7aa21753fd1
geofffactorypure <gjarman@factorypure.com>
2023-08-14 09:23:49 -0500

    adding example 810

--- src/edi/810/components/Example.js
+++ src/edi/810/components/Example.js
@@ -5,23 +5,26 @@ export default function Example() {
             <pre className="whitespace-pre-line text-white text-[14px] p-[8px] bg-darkgrey rounded-[4px] shadow-small overflow-auto">
                 ISA*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*ZZ*VENDOREDIID&nbsp;&nbsp;&nbsp;&nbsp;*ZZ*FACTORYPUREEDIP*230811*1145*U*00400*000000001*O*P*|~<br/>
                 GS*PO*VENDOREDIID*FACTORYPUREEDIP*20230811*1145*1*X*004010~<br/>
-                ST*850*0001~<br/>
-                BEG*00*NE*99999**20230811~<br/>
-                REF*11*123456789~<br/>
-                SAC*A*D980***10000~<br/>
-                N9*L1*MESSAGE TO FOLLOW~<br/>
-                MSG*Customer will need liftgate~<br/>
+                ST*810*0001~<br/>
+                BIG*20230811*99999*20230811*99999***DI~<br/>
+                CUR*SE*USD<br/>
+                REF*BM*123456789~<br/>
                 N1*SF*Vendor Warehouse~<br/>
                 N3*123 St~<br/>
                 N4*CITY*TX*78233*US~<br/>
                 N1*ST*Customer Name~<br/>
-                N3*321 Dr~<br/>
+                N3*456 Dr~<br/>
                 N4*CITY*TX*78704*US~<br/>
+                ITD*05*3****20230811~<br/>
                 FOB*CC*H*WITHLIFTGATE~<br/>
-                TD5*B*2*AACT*T*AAA Cooper~<br/>
-                PO1*001*1*EA*1234.56*PE*CB*FACTORYPURE-SKU*SK*VENDOR-SKU~<br/>
+                IT1*001*1*EA*1234.56*PE***SK*VENDOR-SKU~<br/>
+                PID*F****Inverter generator~<br/>
+                TDS*135802~<br/>
+                TXI*CG*123.46*10*VD*TEXAS****12-4567890~<br/>
+                CAD*T***AACT*AAA Cooper~<br/>
+                ISS*1*EA*215*LB*16268*CI~<br/>
                 CTT*1~<br/>
-                SE*17*0001~<br/>
+                SE*20*0001~<br/>
                 GE*1*1~<br/>
                 IEA*1*000000001~<br/>
             </pre>
