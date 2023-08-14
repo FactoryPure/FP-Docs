a7e57037d548f4ffb5132b1210a5730463af6164
geofffactorypure <gjarman@factorypure.com>
2023-08-14 07:44:10 -0500

    fixing order on po example

--- src/edi/810/components/Example.js
+++ src/edi/810/components/Example.js
@@ -1,10 +1,10 @@
 export default function Example() {
     return (
         <div id="example" className="text-offwhite rounded-[4px] p-[32px] shadow-small bg-darkaccent mt-[32px]">
-            <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Example Purchase Order</h2>
+            <h2 className="text-offwhite border-b pb-[8px] font-bold text-[20px] leading-[1] mb-[16px]">Example Invoice</h2>
             <pre className="whitespace-pre-line text-white text-[14px] p-[8px] bg-darkgrey rounded-[4px] shadow-small overflow-auto">
-                ISA*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*ZZ*FACTORYPUREEDIP*ZZ*VENDOREDIID&nbsp;&nbsp;&nbsp;&nbsp;*230811*1145*U*00400*000000001*O*P*|~<br/>
-                GS*PO*FACTORYPUREEDIP*VENDOREDIID*20230811*1145*1*X*004010~<br/>
+                ISA*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*00*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*ZZ*VENDOREDIID&nbsp;&nbsp;&nbsp;&nbsp;*ZZ*FACTORYPUREEDIP*230811*1145*U*00400*000000001*O*P*|~<br/>
+                GS*PO*VENDOREDIID*FACTORYPUREEDIP*20230811*1145*1*X*004010~<br/>
                 ST*850*0001~<br/>
                 BEG*00*NE*99999**20230811~<br/>
                 REF*11*123456789~<br/>
