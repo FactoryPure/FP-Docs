29fe0646b5fc90fd555454be9141418e6d6d3cf6
geofffactorypure <gjarman@factorypure.com>
2026-04-24 08:41:15 -0700

    adding additional quantity types

--- src/edi/846/components/QTY.js
+++ src/edi/846/components/QTY.js
@@ -43,7 +43,7 @@ export default function QTY() {
                     <div>M</div>
                     <div>ID</div>
                     <div>2/2</div>
-                    <div>V2</div>
+                    <div>17, 33, BA, V2</div>
                 </div>
                 <div className="text-offwhite grid grid-cols-[1fr_1fr_200px_1fr_1fr_1fr_1fr] gap-x-[16px] col-span-full pb-[4px] mb-[8px] border-b border-darkgrey">
                     <div>QTY02</div>
